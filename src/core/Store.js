760980c2df361aa1f21a400ec6556986269d9487
hay <hayhyang@gmail.com>
2024-02-03 18:46:37 +0900

    chore: Store 설명 수정

--- src/core/Store.js
+++ src/core/Store.js
@@ -32,4 +32,4 @@ export default createStore;
 
 // observer패턴과 flux패턴을 혼합하여 redux 구현
 // 기존 redux는 createStore가 subscribe, dispatch, getState 메서드를 가진 객체를 리턴하는 패턴이나
-// subscribe메서드는 Object.defindProperty API로 대체
+// subscribe메서드는 Object.defindProperty API를 활용한 observable, observe 로 대체
