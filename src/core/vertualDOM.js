def32815c7a31c66a96a3fb3a662632a89109447
hay <hayhyang@gmail.com>
2024-02-05 18:05:14 +0900

    feat: vertualDOM 기본형태

---
+++ src/core/vertualDOM.js
@@ -0,0 +1,59 @@
+{
+  /* <div id="app">
+  <ul>
+    <li>
+      <input type="checkbox" class="toggle" />
+      todo list item 1
+      <button class="remove">삭제</button>
+    </li>
+    <li class="completed">
+      <input type="checkbox" class="toggle" checked />
+      todo list item 2
+      <button class="remove">삭제</button>
+    </li>
+  </ul>
+  <form>
+    <input type="text" />
+    <button type="submit">추가</button>
+  </form>
+</div> */
+}
+
+// vertualDOM: DOM의 형태를 본딴 객체
+const vertualDOM = (type, props, ...children) => {
+  return { type, props, children: children.flat() };
+};
+
+const node = vertualDOM(
+  "div",
+  { id: "app" },
+  vertualDOM(
+    "ul",
+    null,
+    vertualDOM(
+      "li",
+      null,
+      vertualDOM("input", { type: "checkbox", class: "toggle" }),
+      "todo list item 1",
+      vertualDOM("button", { class: "remove" }, "삭제")
+    ),
+
+    vertualDOM(
+      "li",
+      null,
+      vertualDOM("input", { type: "checkbox", class: "toggle" }),
+      "todo list item 2",
+      vertualDOM("button", { class: "remove" }, "삭제")
+    )
+  ),
+  vertualDOM(
+    "form",
+    null,
+    vertualDOM("input", { type: "text" }),
+    vertualDOM("button", { type: "submit" }, "추가")
+  )
+);
+
+const createElement = (node) => {};
+
+createElement(node);
